75da6bd07a8f3fa14162773ae6728f33252d2c5e
Austin Hines <Austinhines0605@gmail.com>
2021-04-20 11:22:00 -0500

    Completed components and added styling to the timer and a

---
+++ src/categories/Categories.js
@@ -0,0 +1,20 @@
+const Categories = () => {
+    return (
+        <div className='Categories'>
+            <div className='Categories-Main'>
+                <div className="categories-Main-Top">
+                    <h1>CATEGORIES</h1>
+                    <h1>List 1</h1>
+                </div>
+                <ol className='categories-Main-Display'>
+                    <li>A Boy's Name</li>
+                </ol>
+            </div>
+            <div className="Categories-Secondary">
+                <h1 className='Categories-secondary-H1'>NEXT LIST</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Categories
