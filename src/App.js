2e6c57802e0fd5ca8368891747b10c775cc54d73
Austin Hines <Austinhines0605@gmail.com>
2021-04-23 10:30:32 -0500

    Finished

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import Letter from './letter/Letter'
-import Timer from './timer/Timer'
-import Categories from './categories/Categories';
 import './App.css';
-
+import Letter from './letter/Letter'
+import Timer from './timer/Timer';
+import Categories from './categories/Categories';
+import { useState } from 'react';
 
 function App() {
+  const [hide, setHide] = useState(true);
+
   return (
     <div className="App">
       <div className='App-Section-One'>
-      <Letter/>
-      <Timer/>
+        <Letter />
+        <Timer setHide={setHide} />
       </div>
       <div className='App-Section-Two'>
-      <Categories/>
+        <Categories hide={hide} setHide={setHide} />
       </div>
     </div>
   );
 }
 
 export default App;
+ 
